582a2d056ac57247b7bc02567c71a12857c0248e
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-02-23 04:51:48 +0900

    fix

--- d/example.d
+++ d/example.d
@@ -2,11 +2,15 @@ import std.stdio;
 import std.c.stdlib;
 import gurobi.api_c;
 
+// Error reporting
+void failure(GRBenv* env)
+{
+  writeln("ERROR: ", GRBgeterrormsg(env));
+  exit(1);
+}
+
 void main()
 {
-  GRBenv   *env;
-  GRBmodel *model;
-
   int       error = 0;
   double    sol[3];
   int       ind[3];
@@ -16,102 +20,100 @@ void main()
   int       optimstatus;
   double    objval;
 
-  /* Create environment */
+  // Create environment
+  GRBenv* env;
   error = GRBloadenv(&env, cast(char*)"mip1.log");
-  if (error) goto QUIT;
+  if (error) exit(1);
+  scope(exit) {
+    // Free environment
+    GRBfreeenv(env);
+  }
 
-  /* Create an empty model */
-
+  // Create an empty model
+  GRBmodel* model;
   error = GRBnewmodel(env, &model, cast(char*)"mip1", 0,
           cast(double*)null, cast(double*)null, cast(double*)null,
           cast(char*)null, cast(char**)null);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
+  scope(exit) {
+    // Free model
+    GRBfreemodel(model);
+  }
 
+  // Add variables
+  obj[0] = 1;
+  obj[1] = 1;
+  obj[2] = 2;
 
-  /* Add variables */
+  vtype[0] = GRB_BINARY;
+  vtype[1] = GRB_BINARY;
+  vtype[2] = GRB_BINARY;
 
-  obj[0] = 1; obj[1] = 1; obj[2] = 2;
-  vtype[0] = GRB_BINARY; vtype[1] = GRB_BINARY; vtype[2] = GRB_BINARY;
   error = GRBaddvars(model, 3, 0, cast(int*)null, cast(int*)null, cast(double*)null,
                      cast(double*)obj, cast(double*)null, cast(double*)null,
                      cast(char*)vtype, cast(char**)null);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
-  /* Change objective sense to maximization */
-
+  // Change objective sense to maximization
   error = GRBsetintattr(model, cast(char*)"ModelSense", GRB_MAXIMIZE);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
-  /* Integrate new variables */
-
+  // Integrate new variables
   error = GRBupdatemodel(model);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
 
-  /* First constraint: x + 2 y + 3 z <= 4 */
-
+  // First constraint: x + 2 y + 3 z <= 4
   ind[0] = 0; ind[1] = 1; ind[2] = 2;
   val[0] = 1; val[1] = 2; val[2] = 3;
+  error = GRBaddconstr(model, 3, cast(int*)ind, cast(double*)val,
+                       GRB_LESS_EQUAL, 4.0, cast(char*)"c0");
+  if (error)
+    failure(env);
 
-  error = GRBaddconstr(model, 3, cast(int*)ind, cast(double*)val, GRB_LESS_EQUAL, 4.0, cast(char*)"c0");
-  if (error) goto QUIT;
-
-  /* Second constraint: x + y >= 1 */
-
+  // Second constraint: x + y >= 1
   ind[0] = 0; ind[1] = 1;
   val[0] = 1; val[1] = 1;
+  error = GRBaddconstr(model, 2, cast(int*)ind, cast(double*)val,
+                       GRB_GREATER_EQUAL, 1.0, cast(char*)"c1");
+  if (error)
+     failure(env);
 
-  error = GRBaddconstr(model, 2, cast(int*)ind, cast(double*)val, GRB_GREATER_EQUAL, 1.0, cast(char*)"c1");
-  if (error) goto QUIT;
-
-  /* Optimize model */
-
+  // Optimize model
   error = GRBoptimize(model);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
-  /* Write model to 'mip1.lp' */
-
+  // Write model to 'mip1.lp'
   error = GRBwrite(model, cast(char*)"mip1.lp");
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
-  /* Capture solution information */
-
+  // Capture solution information
   error = GRBgetintattr(model, cast(char*)"Status", &optimstatus);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
   error = GRBgetdblattr(model, cast(char*)"ObjVal", &objval);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
   error = GRBgetdblattrarray(model, cast(char*)"X", 0, 3, cast(double*)sol);
-  if (error) goto QUIT;
+  if (error)
+    failure(env);
 
-  printf("\nOptimization complete\n");
+  writeln();
+  writeln("Optimization complete");
   if (optimstatus == GRB_OPTIMAL) {
     printf("Optimal objective: %.4e\n", objval);
-
     printf("  x=%.0f, y=%.0f, z=%.0f\n", sol[0], sol[1], sol[2]);
   } else if (optimstatus == GRB_INF_OR_UNBD) {
     printf("Model is infeasible or unbounded\n");
   } else {
     printf("Optimization was stopped early\n");
   }
-
-QUIT:
-
-  /* Error reporting */
-
-  if (error) {
-    printf("ERROR: %s\n", GRBgeterrormsg(env));
-    exit(1);
-  }
-
-  /* Free model */
-
-  GRBfreemodel(model);
-
-  /* Free environment */
-
-  GRBfreeenv(env);
-
-  exit(0);
 }
