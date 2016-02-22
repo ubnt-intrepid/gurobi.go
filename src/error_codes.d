71f5671ddca7e06747bec6d0eee00340305f507a
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-02-23 03:32:44 +0900

    initial commit

---
+++ src/error_codes.d
@@ -0,0 +1,32 @@
+// vim: set ft=d ts=2 sw=2 et :
+// Error codes
+
+module error_codes;
+
+const GRB_ERROR_OUT_OF_MEMORY            = 10001;
+const GRB_ERROR_NULL_ARGUMENT            = 10002;
+const GRB_ERROR_INVALID_ARGUMENT         = 10003;
+const GRB_ERROR_UNKNOWN_ATTRIBUTE        = 10004;
+const GRB_ERROR_DATA_NOT_AVAILABLE       = 10005;
+const GRB_ERROR_INDEX_OUT_OF_RANGE       = 10006;
+const GRB_ERROR_UNKNOWN_PARAMETER        = 10007;
+const GRB_ERROR_VALUE_OUT_OF_RANGE       = 10008;
+const GRB_ERROR_NO_LICENSE               = 10009;
+const GRB_ERROR_SIZE_LIMIT_EXCEEDED      = 10010;
+const GRB_ERROR_CALLBACK                 = 10011;
+const GRB_ERROR_FILE_READ                = 10012;
+const GRB_ERROR_FILE_WRITE               = 10013;
+const GRB_ERROR_NUMERIC                  = 10014;
+const GRB_ERROR_IIS_NOT_INFEASIBLE       = 10015;
+const GRB_ERROR_NOT_FOR_MIP              = 10016;
+const GRB_ERROR_OPTIMIZATION_IN_PROGRESS = 10017;
+const GRB_ERROR_DUPLICATES               = 10018;
+const GRB_ERROR_NODEFILE                 = 10019;
+const GRB_ERROR_Q_NOT_PSD                = 10020;
+const GRB_ERROR_QCP_EQUALITY_CONSTRAINT  = 10021;
+const GRB_ERROR_NETWORK                  = 10022;
+const GRB_ERROR_JOB_REJECTED             = 10023;
+const GRB_ERROR_NOT_SUPPORTED            = 10024;
+const GRB_ERROR_EXCEED_2B_NONZEROS       = 10025;
+const GRB_ERROR_INVALID_PIECEWISE_OBJ    = 10026;
+const GRB_ERROR_UPDATEMODE_CHANGE        = 10027;
