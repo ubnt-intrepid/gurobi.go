9bbe9b732539acfc809774ef0d3fa1ebb8879b0b
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-02-23 16:07:22 +0900

    OOP

--- d/example.d
+++ d/example.d
@@ -3,180 +3,199 @@ import std.c.stdlib;
 
 import gurobi.api_c;
 
-// Error reporting
-void failure(GRBenv* env)
-{
-  throw new Exception("ERROR: GUROBI");
-}
 
-GRBenv* loadenv(string logfilename)
+scope class Env
 {
   GRBenv* env;
-  int error = GRBloadenv(&env, cast(char*)logfilename);
-  if (error)
-    throw new Exception("cannot load environment");
-  return env;
+
+public:
+  this(string logfilename)
+  {
+    int error = GRBloadenv(&env, cast(char*)logfilename);
+    if (error)
+      throw new Exception("cannot load environment");
+  }
+
+  ~this()
+  {
+    // Free environment
+    GRBfreeenv(env);
+  }
+
+  GRBenv* get() { return env; }
 }
 
-GRBmodel* newmodel(GRBenv* env, string modelname)
+
+scope class Model
 {
   GRBmodel* model;
-  int error = GRBnewmodel(env, &model, cast(char*)modelname, 0,
-                          cast(double*)null, cast(double*)null,
-                          cast(double*)null, cast(char*)null,
-                          cast(char**)null);
-  if (error)
-    failure(env);
-  return model;
+
+  void failure()
+  {
+    GRBenv* env = GRBgetenv(model);
+    assert(env != null);
+    throw new Exception("ERROR: GUROBI");
+  }
+
+public:
+  this(GRBmodel* model)
+  {
+    this.model = model;
+  }
+
+  this(Env env, string modelname)
+  {
+    GRBmodel* model;
+    int error = GRBnewmodel(env.get(), &model, cast(char*)modelname, 0,
+                            cast(double*)null, cast(double*)null,
+                            cast(double*)null, cast(char*)null,
+                            cast(char**)null);
+    if (error)
+      throw new Exception("ERROR: GUROBI");
+
+    this(model);
+  }
+
+  ~this()
+  {
+    // Free model
+    GRBfreemodel(model);
+  }
+
+  int getintattr(string attr)
+  {
+    int val;
+    int error = GRBgetintattr(model, cast(char*)attr, &val);
+    if (error)
+      failure();
+    return val;
+  }
+
+  double getdblattr(string attr)
+  {
+    double val;
+    int error = GRBgetdblattr(model, cast(char*)attr, &val);
+    if (error)
+      failure();
+    return val;
+  }
+
+  double[] getdblattrarray(string attr, int length)
+  {
+    double[] val = new double[length];
+    int error = GRBgetdblattrarray(model, cast(char*)attr, 0, length, cast(double*)val);
+    if (error)
+      failure();
+    return val;
+  }
+
+  void setintattr(string attr, int value)
+  {
+    int error = GRBsetintattr(model, cast(char*)attr, value);
+    if (error)
+      failure();
+  }
+
+  void addvars(char[] vtype, double[] obj)
+  in {
+    assert(obj.length == vtype.length);
+  }
+  body {
+    int error = GRBaddvars(model, cast(int)obj.length, 0, cast(int*)null, cast(int*)null,
+                           cast(double*)null, cast(double*)obj,
+                           cast(double*)null, cast(double*)null,
+                           cast(char*)vtype, cast(char**)null);
+    if (error)
+      failure();
+  }
+
+  void addvars(char vtype, int length, double[] obj)
+  in {
+    assert(obj.length == length);
+  }
+  body {
+    char[] vtypes = new char[length];
+    vtypes[] = vtype;
+    addvars(vtypes, obj);
+  }
+
+  void addvars(char vtype, int length)
+  {
+    char[] vtypes = new char[length];
+    double[] obj  = new double[length];
+    vtypes[] = vtype;
+    obj[] = 0;
+    addvars(vtypes, obj);
+  }
+
+  void addconstr(int[] ind, double[] val, char sense, double rhs, string cname)
+  in {
+    assert(ind.length == val.length);
+  }
+  body {
+    int error = GRBaddconstr(model, cast(int)ind.length, cast(int*)ind, cast(double*)val,
+                             sense, rhs, cast(char*)cname);
+    if (error)
+      failure();
+  }
+
+  void update()
+  {
+    int error = GRBupdatemodel(model);
+    if (error)
+      failure();
+  }
+
+  void optimize()
+  {
+    int error = GRBoptimize(model);
+    if (error)
+      failure();
+  }
+
+  void write(string filename)
+  {
+    int error = GRBwrite(model, cast(char*)filename);
+    if (error)
+      failure();
+  }
 }
 
-  int getintattr(GRBmodel* model, string attr)
-{
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  int val;
-  int error = GRBgetintattr(model, cast(char*)attr, &val);
-  if (error)
-    failure(env);
-
-  return val;
-}
-
-double getdblattr(GRBmodel* model, string attr)
-{
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  double val;
-  int error = GRBgetdblattr(model, cast(char*)attr, &val);
-  if (error)
-    failure(env);
-
-  return val;
-}
-
-double[] getdblattrarray(GRBmodel* model, string attr, int length)
-{
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  double[] val = new double[length];
-  int error = GRBgetdblattrarray(model, cast(char*)attr, 0, length, cast(double*)val);
-  if (error)
-    failure(env);
-
-  return val;
-}
-
-void setintattr(GRBmodel* model, string attr, int value)
-{
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  int error = GRBsetintattr(model, cast(char*)attr, value);
-  if (error)
-    failure(env);
-}
-
-void addvars(GRBmodel* model, double[] obj, char[] vtype)
-{
-  assert(obj.length == vtype.length);
-
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  int error = GRBaddvars(model, cast(int)obj.length, 0, cast(int*)null, cast(int*)null,
-                         cast(double*)null, cast(double*)obj,
-                         cast(double*)null, cast(double*)null,
-                         cast(char*)vtype, cast(char**)null);
-  if (error)
-    failure(env);
-}
-
-void addconstr(GRBmodel* model, int[] ind, double[] val, char sense, double rhs,
-               string cname)
-{
-  assert(ind.length == val.length);
-
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  int error = GRBaddconstr(model, cast(int)ind.length, cast(int*)ind, cast(double*)val,
-                           sense, rhs, cast(char*)cname);
-  if (error)
-    failure(env);
-}
-
-void updatemodel(GRBmodel* model)
-{
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  int error = GRBupdatemodel(model);
-  if (error)
-    failure(env);
-}
-
-void optimize(GRBmodel* model)
-{
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  int error = GRBoptimize(model);
-  if (error)
-    failure(env);
-}
-
-void write(GRBmodel* model, string filename)
-{
-  GRBenv* env = GRBgetenv(model);
-  assert(env != null);
-
-  int error = GRBwrite(model, cast(char*)filename);
-  if (error)
-    failure(env);
-}
 
 void main()
 {
   // Create environment
-  GRBenv* env = loadenv("mip1.log");
-  scope(exit) {
-    GRBfreeenv(env);  // Free environment
-  }
+  scope env = new Env("mip1.log");
 
   // Create an empty model
-  GRBmodel* model = newmodel(env, "mip1");;
-  scope(exit) {
-    GRBfreemodel(model);  // Free model
-  }
+  scope model = new Model(env, "mip1");;
 
   // Add variables
-  addvars(model, [1, 1, 2], [GRB_BINARY, GRB_BINARY, GRB_BINARY]);
+  model.addvars(GRB_BINARY, 3, /*obj=*/[1, 1, 2]);
+  //model.addvars(GRB_BINARY, 3);
 
   // Change objective sense to maximization
-  setintattr(model, "ModelSense", GRB_MAXIMIZE);
+  model.setintattr("ModelSense", GRB_MAXIMIZE);
 
   // Integrate new variables
-  updatemodel(model);
+  model.update();
 
   // First constraint: x + 2 y + 3 z <= 4
-  addconstr(model, [0, 1, 2], [1, 2, 3], GRB_LESS_EQUAL, 4.0, "c0");
+  model.addconstr([0, 1, 2], [1, 2, 3], GRB_LESS_EQUAL, 4.0, "c0");
 
   // Second constraint: x + y >= 1
-  addconstr(model, [0, 1], [1, 1], GRB_GREATER_EQUAL, 1.0, "c1");
+  model.addconstr([0, 1], [1, 1], GRB_GREATER_EQUAL, 1.0, "c1");
 
   // Optimize model
-  optimize(model);
+  model.optimize();
 
   // Write model to 'mip1.lp'
-  write(model, "mip1.lp");
+  model.write("mip1.lp");
 
   // Capture solution information
-  int optimstatus = getintattr(model, "Status");
-  double objval   = getdblattr(model, "ObjVal");
-  double[] sol    = getdblattrarray(model, "X", 3);
+  int optimstatus = model.getintattr("Status");
+  double objval   = model.getdblattr("ObjVal");
+  double[] sol    = model.getdblattrarray("X", 3);
 
   writeln();
   writeln("Optimization complete");
