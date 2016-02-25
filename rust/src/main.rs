812c8df88ac2565a5d92cfd05f2b0e96da82389a
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-02-26 05:41:21 +0900

    rust1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,21 +1,47 @@
 extern crate libc;
 
-use std::str::FromStr;
+use libc::{c_char, c_int};
 
-struct _GRBenv;
-struct _GRBmodel;
-
+enum _GRBenv {}
 type GRBenv   = _GRBenv;
+
+enum _GRBmodel {}
 type GRBmodel = _GRBmodel;
 
-#[link(name = "gurobi", kind="dylib")]
+#[link(name = "gurobi65", kind="static")]
 extern {
-  fn GRBloadenv(envP: *mut*mut GRBenv, logfilename: libc::c_int)
-      -> libc::c_int;
+  fn GRBloadenv(envP: *mut*mut GRBenv, logfilename: *const c_char) -> c_int;
+  fn GRBfreeenv(env: *mut GRBenv) -> c_int;
+}
+
+
+pub struct Env {
+    env: *mut GRBenv
+}
+
+impl Env {
+
+    pub fn new(logfilename: &str) -> Env
+    {
+        println!("Construct GRBenv");
+        let mut env = 0 as *mut GRBenv;
+        unsafe {
+            GRBloadenv(&mut env, logfilename.to_string().as_ptr() as *const c_char)
+        };
+        Env { env: env }
+    }
+
+}
+
+impl Drop for Env {
+    fn drop(&mut self)
+    {
+        println!("Destruct GRBenv");
+        unsafe { GRBfreeenv(&mut *self.env) };
+    }
 }
 
 fn main() {
-    let mut env = 0 as *mut GRBenv;
-    unsafe { GRBloadenv(&mut env, FromStr::from_str("rust.log").unwrap()); }
-    println!("Hello, rustc");
+    // create a Gurobi environment.
+    let mut env = Env::new("rust.log");
 }
