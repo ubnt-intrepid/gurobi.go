ac0d5b59fbb7e9a7d4993d9d6e5347abd477810f
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-02-27 15:47:24 +0900

    update

--- cpp11/main.cpp
+++ cpp11/main.cpp
@@ -1,3 +1,4 @@
+// $ g++ -std=c++11 main.cpp -lgurobi65 && ./a
 // vim: set ts=2 sw=2 et :
 
 #include <iostream>
@@ -9,65 +10,67 @@ extern "C" {
 #include "gurobi_c.h"
 }
 
-
 class Env {
   friend class Model;
   GRBenv* env = nullptr;
+
 public:
-  Env(std::string const& logfilename = "") {
+  Env(std::string const& logfilename = "")
+  {
     int ret = ::GRBloadenv(&env, logfilename.c_str());
     if (ret) {
       throw std::runtime_error("gurobi");
     }
   }
-  ~Env() {
-    ::GRBfreeenv(env);
-  }
+  ~Env() { ::GRBfreeenv(env); }
 };
-
 
 class Model {
   GRBmodel* model = nullptr;
+
 public:
-  Model(Env& env, std::string const& modelname = "") {
-    int ret = ::GRBnewmodel(env.env, &model, modelname.c_str(),
-                            0, nullptr, nullptr, nullptr, nullptr, nullptr);
+  Model(Env& env, std::string const& modelname = "")
+  {
+    int ret = ::GRBnewmodel(env.env, &model, modelname.c_str(), 0, nullptr,
+                            nullptr, nullptr, nullptr, nullptr);
     if (ret) {
       throw std::runtime_error("GRBnewmodel");
     }
   }
 
-  ~Model() {
-    ::GRBfreemodel(model);
-  }
+  ~Model() { ::GRBfreemodel(model); }
 
-  void update() {
+  void update()
+  {
     int ret = ::GRBupdatemodel(model);
     if (ret) {
       throw std::runtime_error("GRBupdatemodel");
     }
   }
 
-  void optimize() {
+  void optimize()
+  {
     int ret = ::GRBoptimize(model);
     if (ret) {
       throw std::runtime_error("GRBoptimize");
     }
   }
 
-  void add_var(char vtype, std::string const& name,
-               double lb, double ub, double obj = 0.0)
+  template <typename VarType>
+  void add_var(std::string const& name, VarType vtype, double obj = 0.0)
   {
-    int ret = ::GRBaddvar(model, 0, nullptr, nullptr, obj, lb, ub,
-                          vtype, name.c_str());
+    int ret = ::GRBaddvar(model, 0, nullptr, nullptr, obj, vtype.lb(),
+                          vtype.ub(), vtype.vtype(), name.c_str());
     if (ret) {
       throw std::runtime_error("GRBaddvar");
     }
   }
 
   void add_constr(std::string const& name,
-                  std::vector<int> cind, std::vector<double> cval,
-                  char sense, double rhs)
+                  std::vector<int> cind,
+                  std::vector<double> cval,
+                  char sense,
+                  double rhs)
   {
     assert(cind.size() == cval.size());
     int ret = ::GRBaddconstr(model, cind.size(), cind.data(), cval.data(),
@@ -77,7 +80,38 @@ public:
     }
   }
 
-  void set_int_attr(std::string const& attrname, int value) {
+  double get_double_attr(std::string const& attrname)
+  {
+    double value;
+    int ret = ::GRBgetdblattr(model, attrname.c_str(), &value);
+    if (ret) {
+      throw std::runtime_error("GRBgetdblattr");
+    }
+    return value;
+  }
+
+  double get_double_attr(std::string const& attrname, int index)
+  {
+    double value;
+    int ret = ::GRBgetdblattrelement(model, attrname.c_str(), index, &value);
+    if (ret) {
+      throw std::runtime_error("GRBgetdblattrelement");
+    }
+    return value;
+  }
+
+  std::string get_str_attr(std::string const& attrname, int index)
+  {
+    char* value;
+    int ret = ::GRBgetstrattrelement(model, attrname.c_str(), index, &value);
+    if (ret) {
+      throw std::runtime_error("GRBgetstrattrelement");
+    }
+    return value;
+  }
+
+  void set_int_attr(std::string const& attrname, int value)
+  {
     int ret = ::GRBsetintattr(model, attrname.c_str(), value);
     if (ret) {
       throw std::runtime_error("GRBsetintattr");
@@ -85,24 +119,55 @@ public:
   }
 };
 
+struct binary {
+public:
+  constexpr double lb() const { return 0.0; }
+  constexpr double ub() const { return 1.0; }
+  constexpr char vtype() const { return 'B'; }
+};
+
+struct integer {
+  int _lb = -10000;
+  int _ub = 10000;
+
+public:
+  constexpr integer(int lb, int ub)
+      : _lb(lb)
+      , _ub(ub)
+  {
+  }
+  constexpr double lb() const { return _lb; }
+  constexpr double ub() const { return _ub; }
+  constexpr char vtype() const { return 'I'; }
+};
 
 int main(int argc, char const* argv[])
 {
-  Env env;
+  using namespace std;
+
+  Env env("mip1.log");
   Model model(env);
 
-  model.add_var('B', "x", 0.0, 1.0, 1.0);
-  model.add_var('B', "y", 0.0, 1.0, 1.0);
-  model.add_var('B', "z", 0.0, 1.0, 2.0);
+  model.add_var("x", binary{}, 1);
+  model.add_var("y", binary{}, 1);
+  model.add_var("z", binary{}, 2);
+  model.add_var("t", integer{-1000, 1000}, 2);
   model.update();
 
   model.add_constr("c0", {0, 1, 2}, {1, 2, 3}, '<', 4.0);
   model.add_constr("c1", {0, 1}, {1, 2}, '>', 1.0);
+  model.add_constr("c2", {0, 1, 2, 3}, {1, 1, 1, -1}, '=', 0.0);
 
   model.set_int_attr("ModelSense", -1);
   // model.set_objective({ 1, 1, 2 }, -1);
 
   model.optimize();
+  for (int i = 0; i < 4; ++i) {
+    auto vname = model.get_str_attr("VarName", i);
+    auto value = model.get_double_attr("X", i);
+    cout << vname << " = " << value << endl;
+  }
 
-  return 0;
+  double obj = model.get_double_attr("ObjVal");
+  cout << "Obj = " << obj << endl;
 }
