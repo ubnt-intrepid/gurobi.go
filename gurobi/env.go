f7b0cfe290e8443a920a03877ba81f1a58eb67c9
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2022-01-10 18:40:39 -0500

    Added a more general function. SetDBLParam() can be used to set the TimeLimit, Cutoff, etc.

--- gurobi/env.go
+++ gurobi/env.go
@@ -81,3 +81,76 @@ func (env *Env) GetTimeLimit() (float64, error) {
 	return float64(limitOut), nil
 
 }
+
+/*
+SetDBLParam()
+Description:
+	Mirrors the functionality of the GRBsetdblattr() function from the C api.
+	Sets the parameter of the solver that has name paramName with value val.
+*/
+func (env *Env) SetDBLParam(paramName string, val float64) error {
+	// Check that attribute is actually a scalar double attribute.
+	if !IsValidDBLParam(paramName) {
+		return fmt.Errorf("The input attribute name (%v) is not considered a valid attribute.", paramName)
+	}
+
+	// Check that the env object is not nil.
+	if env == nil {
+		return fmt.Errorf("The input env variable to SetTimeLimit() was nil!")
+	}
+
+	// Set Attribute
+	errcode := int(C.GRBsetdblparam(env.env, C.CString(paramName), C.double(val)))
+	if errcode != 0 {
+		return fmt.Errorf("There was an error running GRBsetdblparam(): Error code %v", errcode)
+	}
+
+	// If everything was successful, then return nil.
+	return nil
+
+}
+
+/*
+GetDBLParam()
+Description:
+	Mirrors the functionality of the GRBgetdblattr() function from the C api.
+	Gets the parameter of the model with the name paramName if it exists.
+*/
+func (env *Env) GetDBLParam(paramName string) (float64, error) {
+	// Check the paramName to make sure it is valid
+	if !IsValidDBLParam(paramName) {
+		return -1, fmt.Errorf("The input attribute name (%v) is not considered a valid attribute.", paramName)
+	}
+
+	// Check environment input
+	if env == nil {
+		return -1, fmt.Errorf("The input env variable to SetTimeLimit() was nil!")
+	}
+
+	// Use GRBgetdblparam
+	var valOut C.double
+	errcode := int(C.GRBgetdblparam(env.env, C.CString(paramName), &valOut))
+	if errcode != 0 {
+		return -1, fmt.Errorf("There was an error running GRBgetdblparam(): Error code %v", errcode)
+	}
+
+	// If everything was successful, then return nil.
+	return float64(valOut), nil
+}
+
+func IsValidDBLParam(paramName string) bool {
+	// All param names
+	var scalarDoubleAttributes []string = []string{"TimeLimit", "Cutoff"}
+
+	// Check that attribute is actually a scalar double attribute.
+	paramNameIsValid := false
+
+	for _, validName := range scalarDoubleAttributes {
+		if validName == paramName {
+			paramNameIsValid = true
+			break
+		}
+	}
+
+	return paramNameIsValid
+}
--- gurobi/env_test.go
+++ gurobi/env_test.go
@@ -52,3 +52,70 @@ func TestEnv_SetTimeLimit1(t *testing.T) {
 	}
 
 }
+
+/*
+TestEnv_SetDBLParam1
+Description:
+	Verifies that we can set the value of 'TimeLimit' in current model.
+*/
+func TestEnv_SetDBLParam1(t *testing.T) {
+	// Constants
+	logfilename1 := "thomTide.log"
+	var newTimeLimit float64 = 132
+
+	// Algorithm
+	env, err := NewEnv(logfilename1)
+	if err != nil {
+		t.Errorf("There was an issue creating the new Env variable: %v", err)
+	}
+	defer env.Free()
+
+	err = env.SetDBLParam("TimeLimit", newTimeLimit)
+	if err != nil {
+		t.Errorf("There was an error setting the time limit of the environment! %v", err)
+	}
+
+	detectedTimeLimit, err := env.GetDBLParam("TimeLimit")
+	if err != nil {
+		t.Errorf("There was an error getting the time limit of the environment! %v", err)
+	}
+
+	if detectedTimeLimit != newTimeLimit {
+		t.Errorf("The detected time limit (%v) was not equal to the expected time limit (%v s).", detectedTimeLimit, newTimeLimit)
+	}
+
+}
+
+/*
+TestEnv_SetDBLParam1
+Description:
+	Verifies that we can set the value of 'Cutoff' in current model.
+*/
+func TestEnv_SetDBLParam2(t *testing.T) {
+	// Constants
+	logfilename1 := "thomTide.log"
+	var newTimeLimit float64 = 132
+	var paramToModify string = "Cutoff"
+
+	// Algorithm
+	env, err := NewEnv(logfilename1)
+	if err != nil {
+		t.Errorf("There was an issue creating the new Env variable: %v", err)
+	}
+	defer env.Free()
+
+	err = env.SetDBLParam(paramToModify, 1000.5)
+	if err != nil {
+		t.Errorf("There was an error setting the time limit of the environment! %v", err)
+	}
+
+	detectedTimeLimit, err := env.GetDBLParam(paramToModify)
+	if err != nil {
+		t.Errorf("There was an error getting the time limit of the environment! %v", err)
+	}
+
+	if detectedTimeLimit != newTimeLimit {
+		t.Errorf("The detected time limit (%v) was not equal to the expected time limit (%v s).", detectedTimeLimit, newTimeLimit)
+	}
+
+}
